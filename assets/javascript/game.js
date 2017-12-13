4f2112b6c301fb788b32f673c759612db194ba0a
Julie Groth <groth.julie@gmail.com>
2017-12-12 21:58:29 -0800

    added progress bar functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,11 +5,13 @@ var chosenWordArrayToString = "";
 var guessesRemaining = 10;
 var gamesWon = 0;
 var lettersGuessed = [];
+var progressWidth = "100%";
 
 
 //function to choose word to guess
 function pickANewWord() {
     chosenWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+    console.log(chosenWord);
 }
 pickANewWord();
 
@@ -32,8 +34,7 @@ document.onkeyup = function (event) {
             for (var j = 0; j < chosenWord.length; j++) {
                 if (userGuess === chosenWord[j]) {
                     chosenWordArray[j]=userGuess;
-                    var answerDiv = document.getElementById("current-word");
-                    answerDiv.innerHTML = chosenWordArray.join("  ");
+                    document.getElementById("current-word").innerHTML = chosenWordArray.join("  ");
                 }
             }
             if (chosenWordArray.indexOf("_ ") < 0) {
@@ -46,6 +47,7 @@ document.onkeyup = function (event) {
                lettersGuessed.push(userGuess);
                document.getElementById("letters-guessed").innerHTML  = lettersGuessed.join(", ");
                document.getElementById("guesses-remaining").innerHTML = guessesRemaining; 
+               progressBar();
            }
            else {
             endLoss();
@@ -57,7 +59,7 @@ document.onkeyup = function (event) {
 
 //function for end of game - win
 function endWin () {
-    alert("Yay! You completed a lap! Click the gray button to rehydrate and start a new lap!"); //why does this not work?!?
+    alert("Yay! You completed a lap! Click the gray button to rehydrate and start a new lap!");
     gamesWon += 1;
     document.getElementById("wins").innerHTML = gamesWon;
 }
@@ -74,5 +76,44 @@ function reset(){
     lettersGuessed = [];
     pickANewWord();
     wordToGuess();
-    console.log(chosenWord);
+    progressWidth = "100%";
    };
+
+   //function to change width of progress bar...there is probably a better way to write this, but I couldn't figure out how to iterate %
+   function progressBar() {
+       if(guessesRemaining === 10) {
+            progressWidth = "100%";
+        }
+       if(guessesRemaining === 9) {
+            progressWidth = "90%";
+        }
+        if(guessesRemaining === 8) {
+            progressWidth = "80%";
+        }
+        if(guessesRemaining === 7) {
+            progressWidth = "70%";
+        }
+        if(guessesRemaining === 6) {
+            progressWidth = "60%";
+        }
+        if(guessesRemaining === 5) {
+            progressWidth = "50%";
+        }
+        if(guessesRemaining === 4) {
+            progressWidth = "40%";
+        }
+        if(guessesRemaining === 3) {
+            progressWidth = "30%";
+        }
+        if(guessesRemaining === 2) {
+            progressWidth = "20%";
+        }
+        if(guessesRemaining === 1) {
+            progressWidth = "10%";
+        }
+        if(guessesRemaining === 0) {
+            progressWidth = "0%";
+        }
+          document.getElementById("progress-bar").style.width = progressWidth; 
+          }
+
